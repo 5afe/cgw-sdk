98fa5a61787e9eecae032cfa7ed1c0a923e514d8
iamacook <aaron@safe.global>
2024-08-02 13:02:27 +0200

    Add schema and client for showcase

---
+++ sdk/client.ts
@@ -0,0 +1,9 @@
+/**
+ * This file was auto-generated. Do not make direct changes.
+ */
+
+import _createClient from 'openapi-fetch';
+
+import type { paths } from './schema.d.ts';
+
+export const createClient = _createClient<paths>
